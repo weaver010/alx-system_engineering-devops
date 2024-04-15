2360549d3bb4ba3f4dbd58c05f69717df9b738f6
weaver010 <abdelrahmannagaty839@gmail.com>
2024-04-15 09:00:05 -0700

    hi

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 """ api advanced """
 
-import requests
-
 
 def number_of_subscribers(subreddit):
-    """
-    api advan """
-    base_url = "https://www.reddit.com/r/"
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
 
-    url = "{}{}/about.json".format(base_url, subreddit)
-    headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-                    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 \
-                    Safari/537.36 Edg/123.0.0.0"
-    }
-    results = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if results.status_code == 200:
-        return results.json()["data"]["subscribers"]
-    return 0
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get("data").get("subscribers")
